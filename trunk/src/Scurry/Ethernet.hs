439e98c1afc777324b98a4a37418f7a57be6b1a4
sw17ch <sw17ch@9ffd0b9a-941c-11dd-9ffc-535759b096cd>
2008-10-07 23:39:47 +0000

    Cleaned up the code a bit. Added explicit exports. Added a closeTapHandle to Scurry.TapConfig.

--- trunk/src/Scurry/Ethernet.hs
+++ trunk/src/Scurry/Ethernet.hs
@@ -1,16 +1,14 @@
-module Scurry.Ethernet where
+module Scurry.Ethernet (
+    SrcMAC,
+    DstMAC,
+    EthType,
+    MACAddr(..),
+    EthernetHeader(..),
+) where
 
 import Data.Binary
 import Data.Word
-import qualified Data.ByteString.Lazy as BS
 import Numeric
-
-{- Why oh why does Control.Monad stop at liftM5 -}
-liftM6 :: Monad m => (a1 -> a2 -> a3 -> a4 -> a5 -> a6 -> r) -> m a1 -> m a2 -> m a3 -> m a4 -> m a5 -> m a6 -> m r
-liftM6 f m1 m2 m3 m4 m5 m6 = do
-    { x1 <- m1 ; x2 <- m2 ; x3 <- m3
-    ; x4 <- m4 ; x5 <- m5 ; x6 <- m6
-    ; return (f x1 x2 x3 x4 x5 x6) }
 
 data MACAddr = MACAddr (Word8,Word8,Word8,Word8,Word8,Word8)
 
@@ -47,21 +45,3 @@ instance Binary EthernetHeader where
     put (EthernetHeader d s t) = do
         { put d ; put s ; put t }
 
-type EthernetChecksum = Word32
-type EthernetPayload = BS.ByteString
-
-data EthernetFrame = EthernetFrame EthernetHeader
-                                   EthernetPayload
-                                   EthernetChecksum
-
-instance Show EthernetFrame where
-    show (EthernetFrame h p c) = concat ["{EthFrm ", (show h), " :: ",
-                                         (show $ BS.length p), " = 0x",
-                                         (showHex c $ "}")]
-
-instance Binary EthernetFrame where
-    get = do
-        { h <- get ; p <- get ; c <- get 
-        ; return $ EthernetFrame h p c }
-    put (EthernetFrame h p c) = do
-        { put h ; put p ; put c }
--- trunk/src/Scurry/TapConfig.hs
+++ trunk/src/Scurry/TapConfig.hs
@@ -1,6 +1,9 @@
 {-# OPTIONS -ffi #-}
 
-module Scurry.TapConfig where
+module Scurry.TapConfig(
+    getTapHandle,
+    closeTapHandle
+) where
 
 import Foreign.C.Types
 import System.Posix.Types
@@ -11,13 +14,15 @@ import Network.Socket hiding (send, sendTo, recv, recvFrom)
 getTapHandle :: String -> IO (Either CInt Handle)
 getTapHandle ip_str = (inet_addr ip_str) >>= open_tap
 
+closeTapHandle :: Handle -> IO ()
+closeTapHandle tap = (handleToFd tap) >>= (close_tap_ffi . toEnum . fromEnum)
+
 open_tap :: HostAddress -> IO (Either CInt Handle)
 open_tap addr = do
     tap <- open_tap_ffi (toEnum . fromEnum $ addr)
     if (tap < 0)
         then return (Left tap)
         else do h <- fdToHandle (Fd tap)
-                -- hSetBuffering h NoBuffering
                 return $ Right h
 
 foreign import ccall "help.h open_tap" open_tap_ffi :: CUInt -> IO CInt
