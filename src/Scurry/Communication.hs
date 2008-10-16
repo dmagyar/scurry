9d2c1031b746ab93a4ca745faa68d9788355611c
sw17ch <sw17ch@9ffd0b9a-941c-11dd-9ffc-535759b096cd>
2008-10-16 00:44:06 +0000

    This is the intermediate state before I gut the old threads and replace it with the new system.

--- src/Scurry/Communication.hs
+++ src/Scurry/Communication.hs
@@ -13,11 +13,20 @@ import qualified Data.ByteString as BSS
 import Network.Socket hiding (send, sendTo, recv, recvFrom)
 import Network.Socket.ByteString
 import Control.Monad (forever,liftM,liftM2,liftM4)
+import Control.Concurrent.STM.TChan
+import Data.IORef
+import GHC.Conc
 
 import Scurry.Ethernet
 
 readLength :: Int
 readLength = 1500
+
+data ScurryState = ScurryState [SockAddr]
+
+data DestAddr = DestSingle SockAddr
+              | DestList [SockAddr]
+    deriving (Show)
 
 type FramePair = (EthernetHeader,BS.ByteString)
 
@@ -70,21 +79,30 @@ instance Binary SockAddr where
     put (SockAddrUnix s) = do putWord8 2
                               put s
     
+-- |Bind the socket to the specified socket address.
+-- This specifies the network configuration we're using
+-- as well.
 prepEndPoint :: SockAddr -> IO Socket
 prepEndPoint ep = do
     s <- socket AF_INET Datagram defaultProtocol
     bindSocket s ep
     return s
 
+-- |Reads packets off from the local TAP device
+-- and then writes them to the network sockets
+-- provided.
 localProcessing :: Handle -> Socket -> [SockAddr] -> IO ()
 localProcessing tap net ha = forever $ do
     (readTAP tap) >>= (writeNet net ha)
 
+-- |Reads packets off from the network socket
+-- and writes them to the TAP device.
 remoteProcessing :: Handle -> Socket -> IO ()
 remoteProcessing tap net = forever $ do
     (readNet net) >>= (writeTAP tap)
 
--- |Some functions to do the reading/writing
+-- |Takes an ethernet frame pair and prints some debug
+-- information about it.
 debugFrame :: (EthernetHeader,BS.ByteString) -> IO ()
 debugFrame (h,f) = do
     putStrLn $ concat [(show h)," => Length: ",(show $ BS.length f)]
@@ -111,3 +129,93 @@ writeNet net has (_,frame) = do
 
 bsToEthernetTuple :: BS.ByteString -> (EthernetHeader,BS.ByteString)
 bsToEthernetTuple d = (decode d, d)
+
+
+
+{-
+ - 1. Application Start
+ - 2. Setup TAP device
+ - 3. Create network socket
+ - 4. Create network reading routine
+ - 5. Create TAP reading routine
+ - 6. Create network writing routine (on MVar)
+ - 7. Create TAP writing routine (on MVar)
+ -
+ - Network reader accepts packets, decodes their type,
+ - and then decides what to do with them.
+ -
+ - TAP reading thread encodes the read frame
+ - and then forwards it onto the frame switch.
+ -
+ - Network writer waits on an MVar of type (SockAddr,ByteString)
+ - and writes the string to the provided address.
+ -
+ - The TAP reader waits on an MVar of type (ByteString) and
+ - writes the string to the TAP device.
+ -
+ - The frame switch decides where a packet is going and calls
+ - the necessary writeNet call.
+ -
+ -}
+
+-- |A thread which synchronizes with the switch thread
+-- to read packets from the TAP device and send them 
+-- to the world.
+
+tapSourceThread :: Handle -> (IORef ScurryState) -> (TChan (DestAddr,ScurryMsg)) -> IO ()
+tapSourceThread tap ssRef chan = forever $
+    tapReader tap >>=
+    (\x -> frameSwitch ssRef chan (tapDecode x))
+
+sockWriteThread :: Socket -> (TChan (DestAddr,ScurryMsg)) -> IO ()
+sockWriteThread sock chan = forever $
+    sockWriter sock chan
+
+sockSourceThread :: Handle -> Socket -> (IORef ScurryState) -> (TChan (DestAddr,ScurryMsg)) -> IO ()
+sockSourceThread tap sock ssRef chan = forever $
+    (sockReader sock) >>=
+    (\(addr,msg) -> routeInfo tap ssRef (addr,sockDecode msg))
+    
+
+-- tapReader :: (IORef ScurryState) -> 
+tapReader tap = do
+    hWaitForInput tap (-1)
+    BS.hGetNonBlocking tap readLength
+
+tapDecode :: BS.ByteString -> ScurryMsg
+tapDecode bs = SFrame $ bsToEthernetTuple bs
+
+-- frameSwitch :: ScurryMsg -> IO 
+frameSwitch :: (IORef ScurryState) -> (TChan (DestAddr,ScurryMsg)) -> ScurryMsg -> IO ()
+frameSwitch ssRef chan m = do
+    (ScurryState peers) <- readIORef ssRef
+    mapM_ (\x -> atomically $ writeTChan chan (DestSingle x,m)) peers
+
+-- consoleSVC :: (IORef ScurryState) ->
+-- keepAliveSVC :: (IORef ScurryState) ->
+
+sockWriter :: Socket -> (TChan (DestAddr,ScurryMsg)) -> IO ()
+sockWriter sock chan = do
+    (dst,msg) <- atomically $ readTChan chan
+    case dst of
+         DestSingle addr -> sendTo sock (BSS.concat . BS.toChunks $ encode msg) addr >> return ()
+         _ -> error "Error: I don't support anything except DestSingle yet."
+
+sockReader :: Socket -> IO (SockAddr,BS.ByteString)
+sockReader sock = do
+    (msg,addr) <- recvFrom sock readLength
+    return (addr,BS.fromChunks [msg])
+
+sockDecode :: BS.ByteString -> ScurryMsg
+sockDecode msg = decode msg
+
+routeInfo :: Handle -> (IORef ScurryState) -> (SockAddr,ScurryMsg) -> IO ()
+routeInfo tap ssRef (srcAddr,msg) = do
+    case msg of
+         SFrame (hdr,frame) -> tapWriter tap frame
+         _ -> error "Only SFrame messages are supported at this time."
+
+tapWriter :: Handle -> BS.ByteString -> IO ()
+tapWriter tap frame = do
+    BSS.hPut tap (BSS.concat . BS.toChunks $ frame)
+    hFlush tap
