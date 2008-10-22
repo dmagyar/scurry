a68c0e6584ade0dd1a3a5733dc3c9edfac9c0818
sw17ch <sw17ch@9ffd0b9a-941c-11dd-9ffc-535759b096cd>
2008-10-22 13:11:37 +0000

    Added the 'newpeer' command to the console.

--- trunk/src/Scurry/Console.hs
+++ trunk/src/Scurry/Console.hs
@@ -5,21 +5,38 @@ module Scurry.Console (
 import Control.Concurrent.STM.TChan
 import Control.Monad (forever)
 import Data.IORef
+import Data.List
 -- import GHC.Conc
 import System.IO
 import System.Exit
+import Network.Socket hiding (inet_addr,inet_ntoa)
+import Numeric
 
 import Scurry.Comm.Message
 import Scurry.Comm.Util
+import Scurry.Util
 import Scurry.State
 
 -- Console command interpreter
 
 consoleThread :: (IORef ScurryState) -> (TChan (DestAddr,ScurryMsg)) -> IO ()
 consoleThread ssRef chan = forever $ do
+    let rssr = readIORef ssRef
     ln <- getLine
 
     case ln of
-         "peers" -> (readIORef ssRef) >>= (putStrLn . show)
+         "peers"    -> rssr >>= (putStrLn . show)
          "shutdown" -> exitWith ExitSuccess
-         u -> putStrLn $ "Unknown command: " ++ u
+         "newpeer"  -> putStrLn "Peer: " >> getLine >>= (\l -> atomicModifyIORef ssRef (newPeer l))
+         u          -> putStrLn $ "Unknown command: " ++ u
+
+newPeer :: String -> ScurryState -> (ScurryState, ())
+newPeer addr state = let (ip,port) = break (== ':') addr
+                         port' = drop 1 port
+                         parsed_ip = inet_addr ip
+                         parsed_port = readDec port'
+                         o = case (parsed_ip,parsed_port) of
+                                  (Just ip',[(port'',_)]) -> addAddr state (SockAddrInet port'' ip')
+                                  _ -> state
+                     in (o,())
+    where addAddr (ScurryState peers) sa = ScurryState $ nub (sa : peers)
