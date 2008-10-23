bf9abf7e4390aadb9f552c37edf3381a75df7cca
sw17ch <sw17ch@9ffd0b9a-941c-11dd-9ffc-535759b096cd>
2008-10-23 18:24:06 +0000

    Started adding parsers for the various commands we want to use.

--- src/Scurry/Console/Parser.hs
+++ src/Scurry/Console/Parser.hs
@@ -1,11 +1,44 @@
 module Scurry.Console.Parser (
+    ConsoleCmd(..),
+    parseConsole
 ) where
 
 import Network.Socket (HostAddress,PortNumber)
 import Text.Parsec
+import Text.Parsec.String
 
 data ConsoleCmd = Shutdown
                 | ListPeers
                 | NewPeer HostAddress PortNumber
                 | RemovePeer
     deriving (Show)
+
+parseConsole :: String -> Either ParseError ConsoleCmd
+parseConsole = parse consoleCmd "Console"
+
+consoleCmd :: Parser ConsoleCmd
+consoleCmd = (try cmdShutdown) <|>
+             (try cmdListPeers) <|>
+             (try cmdNewPeer) <|>
+             (try cmdRemovePeer) <|>
+             (fail "Command not recognized")
+
+cmdShutdown :: Parser ConsoleCmd
+cmdShutdown = do
+    string "shutdown"
+    return Shutdown
+
+cmdListPeers :: Parser ConsoleCmd
+cmdListPeers = do
+    string "peers"
+    return ListPeers
+
+cmdNewPeer :: Parser ConsoleCmd
+cmdNewPeer = do
+    string "new"
+    return $ NewPeer 0 0
+
+cmdRemovePeer :: Parser ConsoleCmd
+cmdRemovePeer = do
+    string "remove"
+    return RemovePeer
