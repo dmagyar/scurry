8f6d9c96f97f4620bb229ca9a6cbdc7bf230db7a
sw17ch <sw17ch@9ffd0b9a-941c-11dd-9ffc-535759b096cd>
2008-11-17 21:09:21 +0000

    Added peer list to SJoinReply

--- src/Scurry/Comm/Message.hs
+++ src/Scurry/Comm/Message.hs
@@ -15,15 +15,15 @@ type PingID = Word32
 
 -- |These are the messages we get across the network.
 -- They are the management and data protocol.
-data ScurryMsg = SFrame FramePair       -- | An ethernet frame.
-               | SJoin MACAddr          -- | A network join request.
-               | SJoinReply MACAddr     -- | A network join reply.
-               | SKeepAlive             -- | A keep alive message. 
-               | SNotifyPeer SockAddr   -- | A message to notify others of a peer.
-               | SRequestPeer           -- | A message to request peer listings on the network.
-               | SPing PingID
-               | SEcho PingID
-               | SUnknown               -- | An unknown message
+data ScurryMsg = SFrame FramePair              -- | An ethernet frame.
+               | SJoin MACAddr                 -- | A network join request.
+               | SJoinReply MACAddr [SockAddr] -- | A network join reply.
+               | SKeepAlive                    -- | A keep alive message. 
+               | SNotifyPeer SockAddr          -- | A message to notify others of a peer.
+               | SRequestPeer                  -- | A message to request peer listings on the network.
+               | SPing PingID                  -- | A Ping command used for diagnostics.
+               | SEcho PingID                  -- | A Echo command used to respond to the Ping command.
+               | SUnknown                      -- | An unknown message
     deriving (Show)
 
 type FramePair = (EthernetHeader,BSS.ByteString)
@@ -33,7 +33,7 @@ instance Binary ScurryMsg where
              case tag of
                   0 -> get >>= (return . SFrame)      -- SFrame
                   1 -> get >>= (return . SJoin)       -- SJoin
-                  2 -> get >>= (return . SJoinReply)  -- SJoinReply
+                  2 -> liftM2 SJoinReply get get  -- SJoinReply
                   3 -> return SKeepAlive              -- SKeepAlive
                   4 -> get >>= (return . SNotifyPeer) -- SNotifyPeer
                   5 -> return SRequestPeer            -- SRequestPeer
@@ -41,15 +41,15 @@ instance Binary ScurryMsg where
                   7 -> get >>= (return . SEcho)
                   _ -> return SUnknown                -- Unknown Message
     
-    put (SFrame fp)     = putWord8 0 >> put fp
-    put (SJoin m)       = putWord8 1 >> put m
-    put (SJoinReply m)  = putWord8 2 >> put m
-    put SKeepAlive      = putWord8 3
-    put (SNotifyPeer p) = putWord8 4 >> put p
-    put SRequestPeer    = putWord8 5
-    put (SPing pp)      = putWord8 6 >> put pp
-    put (SEcho pe)      = putWord8 7 >> put pe
-    put SUnknown        = putWord8 255
+    put (SFrame fp)      = putWord8 0 >> put fp
+    put (SJoin m)        = putWord8 1 >> put m
+    put (SJoinReply m p) = putWord8 2 >> put m >> put p
+    put SKeepAlive       = putWord8 3
+    put (SNotifyPeer p)  = putWord8 4 >> put p
+    put SRequestPeer     = putWord8 5
+    put (SPing pp)       = putWord8 6 >> put pp
+    put (SEcho pe)       = putWord8 7 >> put pe
+    put SUnknown         = putWord8 255
 
 instance Binary PortNumber where
     get = liftM PortNum get
