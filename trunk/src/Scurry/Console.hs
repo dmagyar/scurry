a7152dc195b779fe6e0327b9efeaa0572fa0d53f
sw17ch <sw17ch@9ffd0b9a-941c-11dd-9ffc-535759b096cd>
2008-11-17 20:17:26 +0000

    Changed indentation slightly.

--- trunk/src/Scurry/Console.hs
+++ trunk/src/Scurry/Console.hs
@@ -33,10 +33,10 @@ consoleThread ssRef chan = do
              (Right ln') -> goodCmd ln'
 
     where goodCmd cmd = case cmd of
-                            CmdShutdown           -> exitWith ExitSuccess
-                            CmdListPeers          -> (readIORef ssRef) >>= (putStrLn . show)
-                            (CmdNewPeer ha pn)    -> atomicModifyIORef ssRef (newPeer ha pn)
-                            (CmdRemovePeer ha pn) -> atomicModifyIORef ssRef (delPeer ha pn) 
+                             CmdShutdown           -> exitWith ExitSuccess
+                             CmdListPeers          -> (readIORef ssRef) >>= (putStrLn . show)
+                             (CmdNewPeer ha pn)    -> atomicModifyIORef ssRef (newPeer ha pn)
+                             (CmdRemovePeer ha pn) -> atomicModifyIORef ssRef (delPeer ha pn) 
           badCmd err = putStrLn $ "Bad Command: " ++ (show err)
 
 newPeer :: HostAddress -> PortNumber -> ScurryState -> (ScurryState, ())
