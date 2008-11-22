f55dd6e524de6a5a6258a3506ac885946fe464a5
sw17ch <sw17ch@9ffd0b9a-941c-11dd-9ffc-535759b096cd>
2008-11-22 20:21:58 +0000

    Moved a few more pieces to the connection manager. SJoin, SJoinReply, SNotifyPeer are all there now and have been moved from SockSource.

--- trunk/src/Scurry/Comm/ConnectionManager.hs
+++ trunk/src/Scurry/Comm/ConnectionManager.hs
@@ -2,15 +2,17 @@ module Scurry.Comm.ConnectionManager (
     conMgrThread,
 ) where
 
-import Scurry.State
-import Scurry.Types.Threads
+import Data.List (find)
 import Control.Monad (forever)
 import Control.Concurrent.STM
 import GHC.Conc
 
+import Scurry.State
 import Scurry.Util
+import Scurry.Types.Threads
 import Scurry.Types.Network
 import Scurry.Comm.Message
+import Scurry.Comm.Util
 
 -- | (M)anager (M)essage is either a (H)eart (B)eat or a (C)hannel (R)ead
 data MM = HB
@@ -24,12 +26,13 @@ conMgrThread sr swc cmc = do
 
     manage mv >> return ()
 
-    where manage mv = do
-              mv' <- takeMVar mv
-              case mv' of
-                   HB   -> hbHandler sr
-                   CR m -> msgHandler sr m
-              manage mv -- Recursive call
+    where
+        manage mv = do
+            mv' <- takeMVar mv
+            case mv' of
+                HB   -> hbHandler sr
+                CR m -> msgHandler sr swc m
+            manage mv -- Recursive call
 
 
 heartBeatThread :: MVar MM -> IO ()
@@ -45,8 +48,34 @@ chanReadThread mv cmc = forever $ let rd = (atomically $ readTChan cmc)
 hbHandler :: StateRef -> IO ()
 hbHandler sr = return ()
 
-msgHandler :: StateRef -> (EndPoint,ScurryMsg) -> IO ()                                  
-msgHandler sr (ep,sm) = do
+msgHandler :: StateRef -> SockWriterChan -> (EndPoint,ScurryMsg) -> IO ()                                  
+msgHandler sr swc (ep,sm) = do
     case sm of
-        SKeepAlive -> return ()
+        SKeepAlive       -> return ()
+        SJoin mac        -> addPeer sr ((Just mac),ep) >>
+                            joinReply >> joinNotify
+        SJoinReply mac p -> addPeer sr ((Just mac),ep) >>
+                            mapM_ ((addPeer sr) . ((,) Nothing)) p
+        SNotifyPeer np   -> gotNotify np
+
         bad -> error $ "Software Design Error: msgHandler can't use " ++ (show bad)
+
+    where
+        joinReply = do
+            (ScurryState peers _ mymac) <- getState sr        
+            let d = (DestSingle ep)
+                e' = map (\(_,p) -> p) peers
+                p' = filter (/= ep) e'
+                m = SJoinReply mymac p'
+            atomically $ writeTChan swc (d,m)
+        joinNotify = do
+            peers <- getPeers sr
+            let d = (DestList $ filter (/= ep) $ map (\(_,x) -> x) peers)
+                m = (SNotifyPeer ep)
+            atomically $ writeTChan swc (d,m)
+        gotNotify p = do
+            peers <- getPeers sr
+            let e = find (\(_,x) -> x == p) peers
+            case e of
+                 Nothing -> addPeer sr (Nothing,p)
+                 (Just _) -> putStrLn $ "Already have peer " ++ (show p)
