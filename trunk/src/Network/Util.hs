8d7e56e58daddcc0cf6960ea46ab2f8c2e612c59
sw17ch <sw17ch@9ffd0b9a-941c-11dd-9ffc-535759b096cd>
2008-12-26 23:32:27 +0000

    Fixed htonl and friends on windows.

--- trunk/src/Network/Util.hs
+++ trunk/src/Network/Util.hs
@@ -1,4 +1,5 @@
 {-# OPTIONS -XForeignFunctionInterface #-}
+{-# LANGUAGE CPP #-}
 
 module Network.Util (
     htonl,
@@ -9,8 +10,8 @@ module Network.Util (
 
 import Data.Word
 
-foreign import ccall unsafe "htonl" htonl :: Word32 -> Word32 
-foreign import ccall unsafe "htons" htons :: Word16 -> Word16 
-foreign import ccall unsafe "ntohl" ntohl :: Word32 -> Word32
-foreign import ccall unsafe "ntohs" ntohs :: Word16 -> Word16
+foreign import CALLCONV unsafe "htonl" htonl :: Word32 -> Word32 
+foreign import CALLCONV unsafe "htons" htons :: Word16 -> Word16 
+foreign import CALLCONV unsafe "ntohl" ntohl :: Word32 -> Word32
+foreign import CALLCONV unsafe "ntohs" ntohs :: Word16 -> Word16
 
