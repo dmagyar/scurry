45e384fc37ee001a078322d7c0bcdf755dc809ca
sw17ch <sw17ch@9ffd0b9a-941c-11dd-9ffc-535759b096cd>
2009-04-23 03:29:46 +0000

    Added key generation/save/load.

--- src/scurry.hs
+++ src/scurry.hs
@@ -4,20 +4,77 @@ import System.Console.GetOpt
 import System.Directory
 import System.Environment
 
+import Network.Socket
+import OpenSSL
+import OpenSSL.RSA
+import OpenSSL.PEM
+import OpenSSL.EVP.PKey
+
 appName :: String
 appName = "scurry"
 
+-- | Functional 'if'. I can use this with liftM now.
+fif :: Bool -> a -> a -> a
+fif p t f = if p then t
+                 else f
+
+-- | Returns a path to the user data directory
+-- scurry uses to save persistant data like keys
+-- and peer maps.
 getDataDir :: IO FilePath
 getDataDir = do
     p <- getAppUserDataDirectory appName
     createDirectoryIfMissing True p
     return p
+
+pubKeyPath :: FilePath -> FilePath
+pubKeyPath dd = dd ++ "/key.pub"
+
+prvKeyPath :: FilePath -> FilePath
+prvKeyPath dd = dd ++ "/key.prv"
+
+getKey :: FilePath -> IO RSAKeyPair
+getKey dd = do
+    eprv <- doesFileExist (prvKeyPath dd)
+    epub <- doesFileExist (pubKeyPath dd)
+
+    if eprv && epub
+            then ldKey
+            else mkKey
+
+    where
+        mkKey :: IO RSAKeyPair
+        mkKey = do
+            putStrLn $ "Generating RSA keys..."
+            keys <- generateRSAKey' 4096 65537
+            writePKCS8PrivateKey keys Nothing >>= writeFile (prvKeyPath dd) 
+            writePublicKey keys >>= writeFile (pubKeyPath dd)
+            return keys
+
+        ldKey :: IO RSAKeyPair
+        ldKey = do
+            putStrLn $ "Loading RSA keys..."
+            kfl <- readFile (prvKeyPath dd)
+            prv <- readPrivateKey kfl PwNone
+
+            case (toKeyPair prv) of
+                (Just k) -> return k
+                Nothing -> error $ "Bad private key file! Please remove it or fix it (" ++ (prvKeyPath dd) ++ ")"
     
+-- | Make sure everything is setup in the user's
+-- data directory. If it's not setup, do so.
+setup :: IO FilePath
+setup = do
+    dd <- getDataDir
+    return dd
 
 main :: IO ()
-main = do
-    args <- getArgs
-    dd <- getDataDir
+main = withSocketsDo $ withOpenSSL $ do
+    putStrLn "Starting Scurry..."
 
-    putStrLn "Scurry"
-    putStrLn $ "Using data dir: " ++ dd
+    args <- getArgs
+    dd   <- setup
+    key  <- getKey dd
+
+    writePKCS8PrivateKey key Nothing >>= putStrLn
+    writePublicKey key >>= putStrLn
